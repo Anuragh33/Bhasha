fb5295f15134bdf8a6eb931b0344bdddb8e28b6c
Anuragh Ragidimilli <n374r654@wichita.edu>
2025-01-18 23:56:05 -0600

    Update LessonButton.tsx

--- app/(main)/learn/LessonButton.tsx
+++ app/(main)/learn/LessonButton.tsx
@@ -20,5 +20,21 @@ export default function LessonButton({
   const cycleLength = 8
   const cycleIndex = index % cycleLength
 
-  return <div className="">{id}</div>
+  let indentationLevel
+
+  if (cycleIndex <= 2) {
+    indentationLevel = cycleIndex
+  } else if (cycleIndex <= 4) {
+    indentationLevel = 4 - cycleIndex
+  } else if (cycleIndex <= 6) {
+    indentationLevel = 4 - cycleIndex
+  } else {
+    indentationLevel = cycleIndex - 8
+  }
+
+  const rightPosition = indentationLevel * 40
+
+  return
+
+  ;<div className="">{id}</div>
 }
