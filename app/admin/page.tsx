ba4aa5a0b5e15652cc41c8fdae9f28c0057068fe
Anuragh Ragidimilli <n374r654@wichita.edu>
2025-02-18 10:13:53 -0600

    Updtd

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 
 import { getAdmin } from '@/lib/admin'
 
-const App = dynamic(() => import('@/app/admin/app'), { ssr: !!false })
+const App = dynamic(() => import('./app'), { ssr: !!false })
 
 export default function page() {
   const isAdmin = getAdmin()
